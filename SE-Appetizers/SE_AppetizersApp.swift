2745d5b331dc28338515ddb5868e870d7c92b3f7
Önder Koşar <onderkosar@hotmail.com>
2020-11-20 18:32:09 +0300

    Initial Commit

---
+++ SE-Appetizers/SE_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SE_AppetizersApp.swift
+//  SE-Appetizers
+//
+//  Created by Önder Koşar on 20.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SE_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
